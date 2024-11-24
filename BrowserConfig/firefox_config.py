dc4c674a70dc2a51f83bb1fc94a529b01ee173f7
Ratul128 <mehediratul923@gmail.com>
2024-11-24 14:05:39 +0600

    firefox config to selenium

--- BrowserConfig/firefox_config.py
+++ BrowserConfig/firefox_config.py
@@ -1,3 +1,29 @@
-def test():
-    print("test")
-test()
+from selenium import webdriver
+from selenium.webdriver.firefox.service import Service
+import time
+
+
+def firefox_launch () :
+    # Path to GeckoDriver
+    gecko_path = "/Users/nurulamin/Desktop/PythonLearning/geckodriver"
+
+    # Create a Service object for GeckoDriver
+    fDriver_path = Service(gecko_path)
+
+    # Initialize the Firefox driver
+    driver = webdriver.Firefox (service = fDriver_path)
+
+    # Open a website to test
+    driver.get("https://www.google.com")
+
+    # Print the title of the webpage
+    print ( "Page title is:" , driver.title )
+
+    # Keep the browser open for 5 seconds
+    time.sleep(5)
+
+    # Close the browser
+    driver.quit()
+
+# Call the function to launch Firefox
+firefox_launch()
