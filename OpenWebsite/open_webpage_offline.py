d2b2aabc518703d68741f1f37c1494ee4ddcf3a4
Ratul128 <mehediratul923@gmail.com>
2024-11-27 17:53:59 +0600

    open webpage offline practice

---
+++ OpenWebsite/open_webpage_offline.py
@@ -0,0 +1,13 @@
+from selenium import webdriver
+from selenium.webdriver.firefox.service import Service
+
+def offline_webpage_launch():
+    gecho_path="/Users/nurulamin/Desktop/PythonLearning/geckodriver"
+    fDriver_path= Service(gecho_path)
+    driver=webdriver.Firefox(service = fDriver_path)
+
+    driver.get("/Users/nurulamin/Desktop/PythonLearning/romanian-baloch627.htm")
+
+    driver.close()
+
+offline_webpage_launch()
