3af4502d9ab967838c95895eb34a95ef87d57257
Ratul128 <mehediratul923@gmail.com>
2024-12-01 18:04:46 +0600

    webdriver manager practice on firefox

--- BrowserConfig/Webdriver_manager/chrome_wdm.py
+++ BrowserConfig/Webdriver_manager/chrome_wdm.py
@@ -1,9 +1,15 @@
+import time
+
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service as ChromeService
 from webdriver_manager.chrome import ChromeDriverManager
 
 def chrome_webdriver_manager():
     driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()))
+
+    driver.maximize_window()
     driver.get("https://www.google.com/")
+    time.sleep(5)
+    driver.quit()
 
 chrome_webdriver_manager()
