c025f65b09414599dae6be90d04fe590dcea3b21
Ratul128 <mehediratul923@gmail.com>
2024-12-01 17:53:10 +0600

    webdriver manager practice with firefox

---
+++ BrowserConfig/Webdriver_manager/firefox_wdm.py
@@ -0,0 +1,13 @@
+from selenium import webdriver
+from selenium.webdriver.firefox.service import Service as FirefoxService
+from webdriver_manager.firefox import GeckoDriverManager
+
+def firefox_webdriver_manager():
+    driver = webdriver.Firefox(service = FirefoxService(GeckoDriverManager().install()))
+    driver.get("https://www.google.com/")
+
+    driver.maximize_window()
+
+    driver.quit()
+
+firefox_webdriver_manager()
