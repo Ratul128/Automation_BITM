a8231e3ddfb54c7f61534f5db38a6d86de383fb3
Ratul128 <mehediratul923@gmail.com>
2025-07-05 02:33:04 +0600

    firefox config

--- 1_BrowserConfig/firefoxConfig.py
+++ 1_BrowserConfig/firefoxConfig.py
@@ -4,6 +4,7 @@ from selenium.webdriver.firefox.service import Service
 from webdriver_manager.firefox import GeckoDriverManager
 
 def firefox_launch():
-    driver = webdriver.Firefox(service=Service(GeckoDriverManager().install()))
+    driver = webdriver.Firefox(service=Service(GeckoDriverManager().install())) #to config firefox automatically
 
 firefox_launch()
+
